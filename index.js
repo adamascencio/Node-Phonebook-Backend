32b6debd0033e41718cf01170d244ec4ea81494b
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2023-06-13 19:18:39 +0000

    create frontend build

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 app.use(requestLogger);
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
