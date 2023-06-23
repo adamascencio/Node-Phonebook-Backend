fabb0d8017f51ff627179283ff5d19a4abb2c62e
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2023-06-23 00:13:25 +0000

    add custom validator for numbers added by user

--- models/person.js
+++ models/person.js
@@ -1,6 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
+const telephoneCheck = (str) => {
+  const MATCHES = {
+    0: /^\d{3}-\d{3}-\d{3}/,
+    1: /^1*\s*\(\d{3}\)\s*\d{3}-\d{4}/,
+    2: /^\(\d{3}\)\s\d{3}-\d{4}/,
+    3: /\d{3}\s\d{3}\s\d{4}/,
+    4: /^\d{10}$/,
+    5: /^1\s*\d{3}-\d{3}-\d{4}$/,
+  };
+
+  for (const regex in MATCHES) {
+    if (MATCHES[regex].test(str)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.DATABASE_URL)
@@ -10,9 +28,18 @@ mongoose
 const personSchema = new Schema({
   name: {
     type: String,
-    minLength: 3
+    minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return telephoneCheck(v);
+      },
+      message: (props) => `\n${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
